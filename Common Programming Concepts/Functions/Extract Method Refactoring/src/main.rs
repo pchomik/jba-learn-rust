029d4f85aa1089df501a2c808dc4f5f5690bc4d8
Pawel Chomicki <pawel.chomicki@gmail.com>
2026-04-10 22:36:21 +0200

    Complete Functions

--- Common Programming Concepts/Functions/Extract Method Refactoring/src/main.rs
+++ Common Programming Concepts/Functions/Extract Method Refactoring/src/main.rs
@@ -1,7 +1,11 @@
 fn main() {
     let year1993 = 1993;
-    println!("{}: 10 years ago was {}", year1993, year1993 - 10);
+    print_message(year1993, year1993 - 10);
 
     let year2021 = 2021;
-    println!("{}: 10 years ago was {}", year2021, year2021 - 10);
+    print_message(year2021, year2021 - 10);
+}
+
+fn print_message(first_year: i32, second_year: i32 ) {
+    println!("{}: ten years ago was {}", first_year, second_year);
 }
--- Common Programming Concepts/Functions/Passing an Argument/src/main.rs
+++ Common Programming Concepts/Functions/Passing an Argument/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    call_me(TODO);
+    call_me(3);
 }
 
 fn call_me(num: u32) {
