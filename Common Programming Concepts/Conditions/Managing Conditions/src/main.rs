bd2221b6b224fc8e34c0f7308f9cb459bcff7559
Pawel Chomicki <pawel.chomicki@gmail.com>
2026-04-11 21:05:07 +0200

    Complete Conditions

--- Common Programming Concepts/Conditions/Managing Conditions/src/main.rs
+++ Common Programming Concepts/Conditions/Managing Conditions/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let number = 7;
 
-    if !(!(number > 4 && number <= 9) && !(number > 10  && number < 20)) {
+    if !((number <= 4 || number > 9) && (number <= 10 || number >= 20)) {
         print!("If Branch")
     } else {
         print!("Else Branch")
