01bf78ee82615a10a05f67786da4943e07ecb177
Pawel Chomicki <pawel.chomicki@gmail.com>
2026-04-11 22:10:08 +0200

    Complete Tuples and Arrays

--- Common Programming Concepts/Variables, Functions, and Conditions Test/Calculate Price/src/lib.rs
+++ Common Programming Concepts/Variables, Functions, and Conditions Test/Calculate Price/src/lib.rs
@@ -1,3 +1,7 @@
-pub fn calculate_price(/* TODO */) /*TODO*/ {
-    /*write your solution here*/
+pub fn calculate_price(amount: u32) -> u32 {
+    if amount < 40 {
+        amount * 2
+    } else {
+        amount
+    }
 }
