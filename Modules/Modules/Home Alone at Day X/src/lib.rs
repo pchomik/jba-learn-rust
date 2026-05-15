0b16c9503dd9a97116191c5caaf8ad2fd8b2c0ec
Pawel Chomicki <pawel.chomicki@gmail.com>
2026-05-15 22:06:26 +0200

    Complete Modules

--- Modules/Modules/Home Alone at Day X/src/lib.rs
+++ Modules/Modules/Home Alone at Day X/src/lib.rs
@@ -4,8 +4,13 @@ pub mod ground_floor;
 pub mod first_floor;
 pub mod second_floor;
 
+use first_floor::toys_room::i_love_toys;
+use ground_floor::storage_room::i_found_telescope;
+use second_floor::astronomy_tower::i_see_the_stars;
+
 pub fn day_x() {
     i_love_toys();
-    /* What's Alice doing here? */
+    i_found_telescope();
+    i_see_the_stars();
 }
 
