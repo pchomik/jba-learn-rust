f75156f5f410a02b609255c287ec05b97fae8465
Pawel Chomicki <pawel.chomicki@gmail.com>
2026-05-23 21:46:14 +0200

    Complete String

--- Common Collections/Strings/String Transformer/src/lib.rs
+++ Common Collections/Strings/String Transformer/src/lib.rs
@@ -8,9 +8,22 @@ pub mod transformer {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: /* Write type of input */) -> /* Write type of result */ {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         let mut output = vec![];
-        // TODO: Complete the function body!
+        for each in input {
+            match each.1 {
+                Command::Uppercase => output.push(each.0.to_uppercase()),
+                Command::Trim => output.push(each.0.trim().to_string()),
+                Command::Append(num) => {
+                    let mut new_string = String::from(&each.0);
+                    for _ in 0..num {
+                        new_string.push_str("bar");
+                    }
+                    
+                    output.push(new_string);
+                }
+            }
+        }
         output
     }
 }
